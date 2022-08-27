21724ce17770b68fbe0f77a73850a1396145015e
rohithgilla <gillarohith1@gmail.com>
2022-08-27 19:09:03 +0530

    init commit

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,32 @@
+import NextAuth, { type NextAuthOptions } from "next-auth";
+import Auth0Provider from "next-auth/providers/auth0";
+
+// Prisma adapter for NextAuth, optional and can be removed
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "../../../server/db/client";
+import { env } from "../../../env/server.mjs";
+
+export const authOptions: NextAuthOptions = {
+  // Include user.id on session
+  callbacks: {
+    session({ session, user }) {
+      if (session.user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+  },
+  // Configure one or more authentication providers
+  adapter: PrismaAdapter(prisma),
+  secret: env.NEXTAUTH_SECRET,
+  providers: [
+    Auth0Provider({
+      clientId: env.AUTH0_CLIENT_ID,
+      clientSecret: env.AUTH0_CLIENT_SECRET,
+      issuer: env.AUTH0_ISSUER,
+    }),
+    // ...add more providers here
+  ],
+};
+
+export default NextAuth(authOptions);
