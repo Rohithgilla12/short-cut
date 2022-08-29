ed86760ab1423aa1525ee099d89161f229273ad6
rohithgilla <gillarohith1@gmail.com>
2022-08-29 19:19:08 +0530

    Sorted

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,24 @@ const Home: NextPage = () => {
     <BaseLayout title={"Home"}>
       <>
         <main className="container mx-auto flex flex-col items-center justify-center min-h-screen p-4">
+          <div className="w-1/3 prose ">
+            <h3>
+              Short Cut is blazingly fast URL shortner service. It is built on
+              top of redis.
+            </h3>
+            <ul>
+              <li>Provides a great visual analytics of your short links.</li>
+              <li>Fastest redirects to your original URL.</li>
+              <li>No need to wait for the page to load.</li>
+            </ul>
+          </div>
           <CreateShortCutForm />
+          <button
+            onClick={() => userLinks.refetch()}
+            className="btn btn-outline my-4"
+          >
+            Refresh Data
+          </button>
           <div className="overflow-x-auto">
             <table className="table table-zebra w-full">
               <thead>
